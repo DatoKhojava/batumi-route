5667f4a0d90349b0a2c3e9f59b776366b50ebca3
Dato Khojava <khojavad30@gmail.com>
2020-05-27 20:02:04 +0400

    full project

--- screens/TripScreen.js
+++ screens/TripScreen.js
@@ -8,7 +8,6 @@ import {
   TouchableOpacity,
   Text,
   FlatList,
-  CheckBox,
   ScrollView,
   TextInput,
 } from "react-native";
@@ -20,10 +19,16 @@ import { number42 } from "../database/number42";
 import { number44 } from "../database/number44";
 import { number45 } from "../database/number45";
 
-const routes = [number39, number40, number42, number44, number45];
+const routes = [
+  { id: 1, title: "number 39", coords: number39 },
+  { id: 2, title: "numer 40", coords: number40 },
+  { id: 3, title: "number 42", coords: number42 },
+  { id: 4, title: "number 44", coords: number44 },
+  { id: 5, title: "number 45", coords: number45 },
+];
 
 export default function TripScreen() {
-  const [coordinats, setCoordinats] = useState(number45);
+  const [coordinats, setCoordinats] = useState([]);
   const [modalMenu, SetModalMenu] = useState(false);
   const [isSelected, setSelection] = useState(false);
   return (
@@ -33,37 +38,50 @@ export default function TripScreen() {
         style={{ zIndex: 1, position: "absolute", top: 20, right: 30 }}
       >
         <Ionicons name="md-menu" size={35} color="black" />
-        <Modal visible={modalMenu} animationType="slide">
-          <View>
-            <TouchableOpacity>
-              <Ionicons
-                name="ios-close-circle"
-                size={24}
-                color="black"
-                style={{
-                  justifyContent: "center",
-                  textAlign: "center",
-                  margin: 15,
-                }}
-                onPress={() => SetModalMenu(false)}
-              />
-              <View>
-                <FlatList
-                  data={routes}
-                  renderItem={({ item }) => (
-                    <View>
-                      <CheckBox
-                        value={isSelected}
-                        onValueChange={setSelection}
-                      />
-                    </View>
-                  )}
-                />
-              </View>
-            </TouchableOpacity>
-          </View>
-        </Modal>
       </TouchableOpacity>
+      <Modal visible={modalMenu} animationType="slide">
+        <View>
+          <TouchableOpacity>
+            <Ionicons
+              name="ios-close-circle"
+              size={24}
+              color="black"
+              style={{
+                justifyContent: "center",
+                textAlign: "center",
+                margin: 15,
+              }}
+              onPress={() => SetModalMenu(false)}
+            />
+          </TouchableOpacity>
+          <View>
+            <FlatList
+              data={routes}
+              renderItem={({ item, index }) => (
+                <TouchableOpacity
+                  key={item.id}
+                  style={{ margin: 15 }}
+                  onPress={() => {
+                    setCoordinats(item.coords);
+                    SetModalMenu(!modalMenu);
+                  }}
+                >
+                  <View
+                    style={{
+                      flexDirection: "row",
+                    }}
+                  >
+                    <Ionicons name="md-bus" size={24} color="black" />
+                    <Text style={{ fontWeight: "bold", margin: 5 }}>
+                      {item.title}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              )}
+            />
+          </View>
+        </View>
+      </Modal>
       <View>
         <MapView
           provider={PROVIDER_GOOGLE}
@@ -78,7 +96,11 @@ export default function TripScreen() {
             longitudeDelta: 0.0421,
           }}
         >
-          <Polyline coordinates={coordinats} />
+          <Polyline
+            coordinates={coordinats}
+            strokeColor="#333"
+            strokeWidth={4}
+          />
         </MapView>
       </View>
     </View>
@@ -101,21 +123,3 @@ const styles = StyleSheet.create({
     padding: 5,
   },
 });
-
-{
-  /* <MapView
-          provider={PROVIDER_GOOGLE}
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-          initialRegion={{
-            latitude: 41.6168,
-            longitude: 41.6367,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          <Polyline coordinates={coordinats} />
-        </MapView> */
-}
